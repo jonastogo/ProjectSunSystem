86096207814ea43d7c4d46774024cff44c064724
Hator21 <Alexander.Sochart@gmx.de>
2015-12-14 15:20:25 +0100

    Mit mond

--- ProjectSunSystem/src/EllipticalOrbitExample.java
+++ ProjectSunSystem/src/EllipticalOrbitExample.java
@@ -16,81 +16,106 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class EllipticalOrbitExample extends JFrame {
-	Point					size		= new Point();
-	volatile BufferStrategy	bufferStrategy;
+	Point size = new Point();
+	volatile BufferStrategy bufferStrategy;
 
-	Runnable				renderLoop	= new Runnable() {
-											Point	sunPoint	= new Point();
-											Point	center		= new Point();
-											Point	ovalPoint	= new Point();
-											Point	earthPoint	= new Point();
+	Runnable renderLoop = new Runnable() {
+		Point sunPoint = new Point();
+		Point center = new Point();
+		Point ovalPoint = new Point();
+		Point earthPoint = new Point();
+		Point moonPoint = new Point();
 
-											public void run() {
-												center.x = (size.x / 2);
-												center.y = (size.y / 2);
-												sunPoint.x = 200;
-												sunPoint.y = 200;
-												ovalPoint.x = 700;
-												ovalPoint.y = 550;
-												earthPoint.x = 100;
-												earthPoint.y = 100;
-												Image sun = null;
-												try {
-													sun = ImageIO.read(new URL("http://i.imgur.com/85mYqQy.png")).getScaledInstance(sunPoint.x, sunPoint.y, BufferedImage.SCALE_SMOOTH);
-												} catch (Exception e) {
-													e.printStackTrace();
-												}
-												Image earth = null;
-												try {
-													earth = ImageIO.read(new URL("http://i.imgur.com/hvUE093.jpg")).getScaledInstance(earthPoint.x, earthPoint.y, BufferedImage.SCALE_SMOOTH);
-												} catch (Exception e) {
-													e.printStackTrace();
-												}
-												double angle = 0;
-												double step = 0.02;
-												int radiusA = ovalPoint.x / 2;
-												int radiusB = ovalPoint.y / 2;
-												while (true) {
-													Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-													RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-													rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-													g.setRenderingHints(rh);
+		public void run() {
+			center.x = (size.x / 2);
+			center.y = (size.y / 2);
+			sunPoint.x = 200;
+			sunPoint.y = 200;
+			ovalPoint.x = 700;
+			ovalPoint.y = 550;
+			earthPoint.x = 100;
+			earthPoint.y = 100;
+			moonPoint.x = 10;
+			moonPoint.y = 10;
 
-													g.setBackground(Color.black);
+			Image sun = null;
+			try {
+				sun = ImageIO.read(new URL("http://i.imgur.com/85mYqQy.png")).getScaledInstance(sunPoint.x, sunPoint.y,
+						BufferedImage.SCALE_SMOOTH);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Image earth = null;
+			try {
+				earth = ImageIO
+						.read(new URL(
+								"http://img1.wikia.nocookie.net/__cb20131031215453/dc-cinematic-universe/de/images/e/e1/Erde.png"))
+						.getScaledInstance(earthPoint.x, earthPoint.y, BufferedImage.SCALE_SMOOTH);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Image moon = null;
+			try {
+				moon = ImageIO
+						.read(new URL(
+								"http://orig13.deviantart.net/957e/f/2011/363/9/9/moon_png_by_natyjonasproductions-d4kod97.png"))
+						.getScaledInstance(moonPoint.x, moonPoint.y, BufferedImage.SCALE_SMOOTH);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			double angle = 0;
+			double step = 0.02;
+			int radiusA = ovalPoint.x / 2;
+			int radiusB = ovalPoint.y / 2;
+			while (true) {
+				Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+				RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_ON);
+				rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+				g.setRenderingHints(rh);
 
-													int x = (int) (radiusA * Math.sin(angle)) + center.x;
-													int y = (int) (radiusB * Math.cos(angle)) + center.y;
+				g.setBackground(Color.black);
 
-													g.clearRect(0, 0, getWidth(), getHeight());
+				int x = (int) (radiusA * Math.sin(angle)) + center.x;
+				int y = (int) (radiusB * Math.cos(angle)) + center.y;
 
-													g.setColor(Color.yellow);
-													g.drawImage(sun, (center.x) - (sunPoint.x / 2), (center.y) - (sunPoint.y / 2), null);
+				g.clearRect(0, 0, getWidth(), getHeight());
+				//Sonne
+				g.setColor(Color.yellow);
+				g.drawImage(sun, (center.x) - (sunPoint.x / 2), (center.y) - (sunPoint.y / 2), null);
+				//Erdumlaufbahn
+				g.setColor(Color.white);
+				((Graphics2D) g).setStroke(new BasicStroke(3));
+				g.drawOval((size.x / 2) - (ovalPoint.x / 2), (size.y / 2) - (ovalPoint.y / 2), ovalPoint.x,
+						ovalPoint.y);
 
-													g.setColor(Color.white);
-													((Graphics2D) g).setStroke(new BasicStroke(3));
-													g.drawOval((size.x / 2) - (ovalPoint.x / 2), (size.y / 2) - (ovalPoint.y / 2), ovalPoint.x, ovalPoint.y);
+				((Graphics2D) g).setStroke(new BasicStroke(1));
+				//Erde
+				g.setColor(Color.blue);
+				g.drawImage(earth, x - earthPoint.x / 2, y - earthPoint.y / 2, null);
+			
+				int mx=(int) (Math.cos(angle)*(earthPoint.x+20)+x);
+				int my=(int) (Math.cos(angle)*(earthPoint.y+20)+y);
+				
+				g.setColor(Color.white);
+				g.drawImage(moon,mx-(moonPoint.x/2),my-(moonPoint.y/2), null);
 
-													((Graphics2D) g).setStroke(new BasicStroke(1));
+				bufferStrategy.show();
 
-													g.setColor(Color.blue);
-													g.drawImage(earth, x - earthPoint.x / 2, y - earthPoint.y / 2, null);
+				g.dispose();
 
-													bufferStrategy.show();
+				try {
+					TimeUnit.MILLISECONDS.sleep(40);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
-													g.dispose();
+				angle += step;
+				angle %= 360;
+			}
 
-													try {
-														TimeUnit.MILLISECONDS.sleep(40);
-													} catch (InterruptedException e) {
-														e.printStackTrace();
-													}
-
-													angle += step;
-													angle %= 360;
-												}
-
-											}
-										};
+		}
+	};
 
 	public EllipticalOrbitExample() {
 		super("EllipticalOrbitExample");
